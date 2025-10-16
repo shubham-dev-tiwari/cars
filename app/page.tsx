e14e3a92fb4e5a20794fd01a46d4b1b118f95edb
shubham-dev <97054735+shubham-dev-tiwari@users.noreply.github.com>
2025-10-16 13:03:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,32 @@
-import { fetchCars } from "@utils";
-import { HomeProps } from "@types";
-import { fuels, yearsOfProduction } from "@constants";
-import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
+import { Hero, SearchBar, CustomFilter, CarCard } from '@components';
 
-export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
-
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
+export default async function Home() {
   return (
-    <main className='overflow-hidden'>
+    <main className="overflow-hidden">
       <Hero />
-
+      
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Autobots</h1>
-          <p>Get your autobot now</p>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
         </div>
 
         <div className='home__filters'>
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} />
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
           </div>
         </div>
 
-        {!isDataEmpty ? (
-          <section>
-            <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
-              ))}
-            </div>
-
-            <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
-            />
-          </section>
-        ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-            <p>{allCars?.message}</p>
+        <section>
+          <div className='home__cars-wrapper'>
+            {/* Cars will be displayed here */}
+            <p>Loading cars...</p>
           </div>
-        )}
+        </section>
       </div>
     </main>
-  );
+  )
 }
